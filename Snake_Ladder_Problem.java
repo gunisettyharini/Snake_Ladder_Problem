3df31d22b40d18941872fb03d490a39bfdb0c9c7
Harini Gunisetty <gunisettyharini@gmail.com>
2021-02-11 21:41:49 +0530

    Welcome to Snake and ladder program

---
+++ Snake_Ladder_Problem.java
@@ -0,0 +1,8 @@
+public class SnakeLadder {
+	final static int PLAYER_1=0;
+	public static void main(String args[])
+	{
+	System.out.println("Start the game");
+	System.out.println("Player1 Position is "+PLAYER_1);
+	}
+}
